bfd945ed8904c29c68b75524d9273b1623a44ecf
Gilbert <juliegil@UMICH.EDU>
2021-05-25 14:54:34 -0400

    added comments

--- OutsidePipeline/ProcessingFASTA/prep_fasta_for_gisaid.py
+++ OutsidePipeline/ProcessingFASTA/prep_fasta_for_gisaid.py
@@ -34,16 +34,20 @@ def main():
 
     ### This section converts barcode (NBXX) into sample_id.
 
+    # create set of file names
     file_1 = args.prefix + ".all.consensus.fasta"
     file_2 = args.prefix + ".all.consensus.tmp.fasta"
     file_3 = args.prefix + ".all.consensus.renamed.full.fasta" # This is the file to use in pangolin and nextclade.
     meta_file = args.prefix + ".meta.csv" # This is the full compiled list, or the subset that matches this run.
 
+    # read in meta file, which is the compiled file (full_compiled_data.csv)
     meta = pd.read_csv(meta_file, index_col = None, header = 0, dtype = object)
     meta.columns = meta.columns.astype(str)
 
     # Change NBXX into sample_id
     all_fasta = list()
+    # read in file_1, which is the output from the lab (fastq -> fasta)
+    # replace barcode as ID part with the corresponding sample_id
     for record in SeqIO.parse(file_1, "fasta"):
 
         id = str(record.id).split()[0]
@@ -55,13 +59,14 @@ def main():
         record.id = new_ID
         all_fasta.append(record)
 
-    # Write everything
+    # Write everything out as file_2 name, with the replaced system
     with open(file_2, 'w') as corrected:
         SeqIO.write(all_fasta, corrected, "fasta")
+    # turn it into file_3 name
     sed_cmd = """ sed '/^>/ s/ .*//' """ + file_2 + " > " + file_3
     print(sed_cmd)
     os.system(sed_cmd)
-    os.system("rm " + file_2)
+    os.system("rm " + file_2) # remove the file_2 name
 
     ### Make filtered fasta file ready for GISAID. Length filtering step occurred in Nanopore script in lab (90% completeness, or 27000 bases).
 

6702fa28c152cad5078281ea20e95881f84a51ac
Gilbert <juliegil@UMICH.EDU>
2021-06-02 11:09:59 -0400

    new subsetting code

---
+++ OutsidePipeline/subset_compiled_for_fasta.R
@@ -0,0 +1,24 @@
+################################################################################
+#       Creation of Subsetting Compiled File for FASTA Name Replacement        #
+#                         Last Updated: 06/02/2021                             #
+#                 Code Edited By: Julie (Jules) Gilbert                        #
+################################################################################
+
+# load libraries
+library(tidyverse)
+library(lubridate)
+
+################################################################################
+# set paths 
+starting_path <- "C:/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/"
+## set run folder accordingly
+outputLOC <- paste0(starting_path, "ProcessedGenomes/20210524_Nanopore_Run_27/")
+
+# read in compiled dataset
+seq_list <- read.csv(paste0(starting_path, "SequenceSampleMetadata/FinalSummary/full_compiled_data.csv"), colClasses = "character")
+
+################################################################################
+# filter to plate run
+seq_list2 <- filter(seq_list, PlateNumber == "27")
+# write out that file as the .meta.csv file - change name as appropriate
+write.csv(seq_list2, paste0(outputLOC, "20210524_Nanopore_Run_27.meta.csv"), row.names = FALSE, na = "")

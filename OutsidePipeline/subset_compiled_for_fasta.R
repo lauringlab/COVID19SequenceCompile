071114e47ecf568d346b4b0e858d3a0a701fe422
Gilbert <juliegil@UMICH.EDU>
2021-08-09 07:44:41 -0400

    most up to date plate run info added to vars

--- OutsidePipeline/subset_compiled_for_fasta.R
+++ OutsidePipeline/subset_compiled_for_fasta.R
@@ -12,13 +12,13 @@ library(lubridate)
 # set paths 
 starting_path <- "C:/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/"
 ## set run folder accordingly
-outputLOC <- paste0(starting_path, "ProcessedGenomes/20210528_Nanopore_Run_28/")
+outputLOC <- paste0(starting_path, "ProcessedGenomes/20210803_Nanopore_Run_37/")
 
 # read in compiled dataset
 seq_list <- read.csv(paste0(starting_path, "SequenceSampleMetadata/FinalSummary/full_compiled_data.csv"), colClasses = "character")
 
 ################################################################################
 # filter to plate run
-seq_list2 <- filter(seq_list, PlateNumber == "28")
+seq_list2 <- filter(seq_list, PlateNumber == "37")
 # write out that file as the .meta.csv file - change name as appropriate
-write.csv(seq_list2, paste0(outputLOC, "20210528_Nanopore_Run_28.meta.csv"), row.names = FALSE, na = "")
+write.csv(seq_list2, paste0(outputLOC, "20210803_Nanopore_Run_37.meta.csv"), row.names = FALSE, na = "")

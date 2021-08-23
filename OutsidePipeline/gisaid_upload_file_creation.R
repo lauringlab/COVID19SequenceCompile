457317fa0ccb594b60d397f0d4929155532acc06
Gilbert <juliegil@UMICH.EDU>
2021-08-23 08:27:36 -0400

    just file name change with new run

--- OutsidePipeline/gisaid_upload_file_creation.R
+++ OutsidePipeline/gisaid_upload_file_creation.R
@@ -19,9 +19,9 @@ starting_path <- "C:/Users/juliegil/Dropbox (University of Michigan)/MED-Lauring
 ################################################################################
 ### fill in some info manually
 
-plate_datef <- "20210809" # plate date in YYYYMMDD format
+plate_datef <- "20210811" # plate date in YYYYMMDD format
 runtech <- "Nanopore" # nanopore or illumina, will match "PlatePlatform" options
-runnum <- "38" # number, will match "PlateNumber" options
+runnum <- "39" # number, will match "PlateNumber" options
 
 ################################################################################
 

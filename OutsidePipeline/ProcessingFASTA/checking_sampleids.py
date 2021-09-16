0c107ae033ccefd93b8d3817f6b7b1f6830f43e4
Gilbert <juliegil@UMICH.EDU>
2021-09-16 08:34:27 -0400

    have two lists of sample_ids

--- OutsidePipeline/ProcessingFASTA/checking_sampleids.py
+++ OutsidePipeline/ProcessingFASTA/checking_sampleids.py
@@ -1,6 +1,12 @@
 ## Checking fasta file matches to plates
 ## expectation = the same
+### Jules
+
 import pandas as pd
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
 ## Process = samples have barcode map that assigns sample_ids to each sample
 ## sequence. If the barcode map is incorrect (contains the wrong samples) then
 ## the sequences will be matched with the wrong samples
@@ -10,7 +16,20 @@ meta_file_location = "C:/Users/juliegil/Dropbox (University of Michigan)/MED-Lau
 meta_file = "20210908_Nanopore_Run_48.meta.csv"
 meta = pd.read_csv(meta_file_location + meta_file, index_col = None, header = 0, dtype = object)
 
-print(meta['sample_id'])
+plate_sampleids = meta['sample_id'].tolist()
+
 ## second, get a list of all the sample_ids in the fasta file we're comparing it to
+fasta_file = "20210908_Nanopore_Run_48.all.consensus.renamed.full.fasta"
+file_1 = meta_file_location + fasta_file
+
+fasta_file_sampleids = list()
+
+for record in SeqIO.parse(file_1, "fasta"):
+
+    id = str(record.id).split()[0]
+    id = id.split("/", 1)[0] ## removes excess info that comes through with barcode in some instances (NB01/ARTIC/nanopolish)
+
+    fasta_file_sampleids.append(id)
+
 
 ## print out -- all matches OR some different (list them) OR all different

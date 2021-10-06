174a5d9425df4e2eea19eb3c3ad00fe12003514f
Gilbert <juliegil@UMICH.EDU>
2021-10-06 07:27:00 -0400

    new file names for most recent run

--- OutsidePipeline/ProcessingFASTA/prep_fasta_NumberTwo.py
+++ OutsidePipeline/ProcessingFASTA/prep_fasta_NumberTwo.py
@@ -6,7 +6,7 @@
 
 #"""
 # use in git bash
-#c:/users/juliegil/appdata/local/programs/python/python38/python.exe C:/Users/juliegil/Documents/UofM_Work/SequenceCompilationCode/OutsidePipeline/ProcessingFASTA/prep_fasta_NumberTwo.py --prefix "C:/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/3_ProcessedGenomes/20210913_Nanopore_Run_49/20210913_Nanopore_Run_49"
+#c:/users/juliegil/appdata/local/programs/python/python38/python.exe C:/Users/juliegil/Documents/UofM_Work/SequenceCompilationCode/OutsidePipeline/ProcessingFASTA/prep_fasta_NumberTwo.py --prefix "C:/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/3_ProcessedGenomes/20210929_Nanopore_Run_57/20210929_Nanopore_Run_57"
 # python prep_fasta_NumberTwo.py --prefix 20210519_Nanopore_Run_26
 #"""
 
@@ -54,6 +54,7 @@ def main():
 
         id = str(record.id).split()[0]
         id = id.split("/", 1)[0] ## removes excess info that comes through with barcode in some instances (NB01/ARTIC/nanopolish)
+        #id = str(id).upper()
         print(id)
 
         if(id in IDs):

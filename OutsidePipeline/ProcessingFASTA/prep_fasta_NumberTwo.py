749a44375496b3c14d4f55584a7f89ca6423847c
Gilbert <juliegil@UMICH.EDU>
2021-12-08 09:44:43 -0500

    general updates

--- OutsidePipeline/ProcessingFASTA/prep_fasta_NumberTwo.py
+++ OutsidePipeline/ProcessingFASTA/prep_fasta_NumberTwo.py
@@ -6,7 +6,7 @@
 
 #"""
 # use in git bash
-#c:/users/juliegil/appdata/local/programs/python/python38/python.exe C:/Users/juliegil/Documents/UofM_Work/SequenceCompilationCode/OutsidePipeline/ProcessingFASTA/prep_fasta_NumberTwo.py --prefix "C:/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/3_ProcessedGenomes/20211123_SC2_Nanopore_Run_78/20211123_SC2_Nanopore_Run_78"
+#c:/users/juliegil/appdata/local/programs/python/python38/python.exe C:/Users/juliegil/Documents/UofM_Work/SequenceCompilationCode/OutsidePipeline/ProcessingFASTA/prep_fasta_NumberTwo.py --prefix "C:/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/3_ProcessedGenomes/20211202_SC2_Nanopore_Run_82/20211202_SC2_Nanopore_Run_82"
 # python prep_fasta_NumberTwo.py --prefix 20210519_Nanopore_Run_26
 #"""
 
--- OutsidePipeline/gisaid_upload_file_creation.R
+++ OutsidePipeline/gisaid_upload_file_creation.R
@@ -19,9 +19,9 @@ starting_path <- "C:/Users/juliegil/Dropbox (University of Michigan)/MED-Lauring
 ################################################################################
 ### fill in some info manually
 
-plate_datef <- "20211129" # plate date in YYYYMMDD format
+plate_datef <- "20211202" # plate date in YYYYMMDD format
 runtech <- "Nanopore" # nanopore or illumina, will match "PlatePlatform" options
-runnum <- "79" # number, will match "PlateNumber" options
+runnum <- "82" # number, will match "PlateNumber" options
 
 ################################################################################
 

72510bda3421ef4eb607681346c73df0dfcd5c47
Gilbert <juliegil@UMICH.EDU>
2022-02-03 14:20:26 -0500

    move and edit plate map excel file done

--- OutsidePipeline/moving_plate_map_files.R
+++ OutsidePipeline/moving_plate_map_files.R
@@ -6,8 +6,25 @@ library(openxlsx)
 # and drop it in
 # Dropbox (University of Michigan)\MED-LauringLab\SEQUENCING\SARSCOV2\4_SequenceSampleMetadata\PlateMaps
 
-paste0(starting_path, "SEQUENCING/SARSCOV2/2_PlateMaps/", plate_name, ".xlsx")
-
 file.copy(from = paste0(starting_path, "SEQUENCING/SARSCOV2/2_PlateMaps/", plate_name, ".xlsx"),
             to = paste0(starting_path, "SEQUENCING/SARSCOV2/4_SequenceSampleMetadata/PlateMaps/", plate_name, ".xlsx"))
 
+
+# then, read in that excel file that we moved to the inner pipeline set
+file_in <- read.xlsx(paste0(starting_path, "SEQUENCING/SARSCOV2/4_SequenceSampleMetadata/PlateMaps/", plate_name, ".xlsx"), sheet = 1)
+
+# remove the well position column
+drop_columns <- c("Well.Position")
+file_in <- file_in[!names(file_in) %in% drop_columns]
+
+# Rename first 7 columns
+colnames(file_in)[1] <- "Processing Plate"
+colnames(file_in)[2] <- "Slot"
+colnames(file_in)[3] <- "Sample ACCN"
+colnames(file_in)[4] <- "Sample MRN"
+colnames(file_in)[5] <- "Sample Order#"
+colnames(file_in)[6] <- "Barcode"
+colnames(file_in)[7] <- "Source"
+
+# write the excel file back out
+write.xlsx(file_in, paste0(starting_path, "SEQUENCING/SARSCOV2/4_SequenceSampleMetadata/PlateMaps/", plate_name, ".xlsx"), overwrite = TRUE)

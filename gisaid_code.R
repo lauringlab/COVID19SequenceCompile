b5b11f2d57268beb84b022e7a6b88ff508443e28
Julie Gilbert <juliegil@m-h4tgx16rpn7c.wifi.med.umich.edu>
2022-04-21 09:50:55 -0400

    new update for addtional gisaid columns

--- gisaid_code.R
+++ gisaid_code.R
@@ -35,7 +35,7 @@ gisaid_storage <- read.delim(paste0(gisaid_fp, "/", file_list[1]))
 gisaid_storage <- remove_empty(gisaid_storage)
   
 # select columns we care about
-gisaid_storage <- gisaid_storage %>% select(Virus.name, Accession.ID)
+gisaid_storage <- gisaid_storage %>% select(Virus.name, Accession.ID, Clade, Pango.lineage)
 
 # create sample_id column
 gisaid_storage$sample_id <-  sapply(strsplit(as.character(gisaid_storage$Virus.name),'/'), "[", 3)
@@ -43,7 +43,7 @@ gisaid_storage <- filter(gisaid_storage, grepl("MI-UM", Virus.name) | grepl("IVY
 gisaid_storage$sample_id <-  sapply(strsplit(as.character(gisaid_storage$sample_id),'-'), "[", 3)
 
 ### rename columns 
-rename_columns <- c("gisaid_strain", "gisaid_epi_isl", "sample_id")
+rename_columns <- c("gisaid_strain", "gisaid_epi_isl", "gisaid_clade", "gisaid_pango_lineage", "sample_id")
 colnames(gisaid_storage) <- rename_columns
 
 # write out the compiled file

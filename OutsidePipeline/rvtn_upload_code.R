3d156293d7c6d2b20d5d5920a556f1ca9875ed04
Julie Gilbert <juliegil@m-h4tgx16rpn7c.wifi.med.umich.edu>
2022-10-19 12:48:41 -0400

    variable update for rvtn uploads to redcap

--- OutsidePipeline/rvtn_upload_code.R
+++ OutsidePipeline/rvtn_upload_code.R
@@ -24,11 +24,11 @@ seq_list <- seq_list %>% select(sample_id, subject_id, coll_date,
                                 nextclade_completeness, gisaid_strain, gisaid_epi_isl,              
                                 received_date, position,          
                                 PlateName, PlatePosition, SampleSourceLocation,        
-                                pangoLEARN_version, pangolin_conflict, pango_version,               
-                                pangolin_version, pangolin_runDate, PlateToPangolin_days,        
+                                pangoLEARN_version, pango_version,               
+                                pangolin_version, pangolin_conflict,     
                                 nextclade_qcOverallScore, nextclade_qcOverallStatus, nextclade_totalMutations,    
-                                nextclade_totalNonACGTNs, nextclade_runDate, PlateToNextclade_days)#, sample_id_lauring, 
-                                #data_quality_rule, newest_pangolin_lineage, newest_pangolin_date)
+                                nextclade_totalNonACGTNs, 
+                                data_quality_rule, newest_pangolin_lineage, newest_pangolin_date, sample_id_lauring)
 
 seq_list <- filter(seq_list, received_source == "RVTN" & !grepl("Missing Date", flag))
 
@@ -44,7 +44,7 @@ seq_list <- seq_list %>% mutate(coll_date = case_when(grepl("/", coll_date) ~ as
                                           T ~ NA_character_))
 
 
-colnames(seq_list) <- tolower(colnames(seq_list))
+#colnames(seq_list) <- tolower(colnames(seq_list))
 
 # add leading zero to month
 if (nchar(month(Sys.Date())) == 1){
@@ -62,16 +62,32 @@ if (nchar(day(Sys.Date())) == 1){
 today <- paste0(year(Sys.Date()), m, d)
 
 rvtn <- seq_list %>% select(sample_id, subject_id, coll_date,                    
-                                flag, received_source, sitename, samplebarcode,                
-                                platedate, plateplatform, platenumber, 
-                            pangolin_lineage, pangolin_status,             
-                            pangolin_note, nextclade_clade, nextclade_totalmissing,      
-                            nextclade_completeness, gisaid_strain, gisaid_epi_isl,
-                            received_date, position, platename, plateposition, samplesourcelocation,
-                                 pangolearn_version,
-                                pango_version, pangolin_version,  nextclade_qcoverallscore, nextclade_qcoverallstatus, 
-                                nextclade_totalmutations, nextclade_totalnonacgtns)#, sample_id_lauring, 
-                            #data_quality_rule, newest_pangolin_lineage, newest_pangolin_date)
+                            flag, received_source, SiteName, SampleBarcode,                
+                            PlateDate, PlatePlatform, PlateNumber,                 
+                            pangolin_lineage, pangolin_probability, pangolin_status,             
+                            pangolin_note, nextclade_clade, nextclade_totalMissing,      
+                            nextclade_completeness, gisaid_strain, gisaid_epi_isl,              
+                            received_date, position,          
+                            PlateName, PlatePosition, SampleSourceLocation,        
+                            pangoLEARN_version, pango_version,               
+                            pangolin_version, pangolin_conflict,     
+                            nextclade_qcOverallScore, nextclade_qcOverallStatus, nextclade_totalMutations,    
+                            nextclade_totalNonACGTNs, 
+                            data_quality_rule, newest_pangolin_lineage, newest_pangolin_date, sample_id_lauring)
+
+colnames(rvtn) <- c("sample_id", "subject_id", "coll_date",                    
+                    "flag", "received_source", "sitename", "samplebarcode",                
+                    "platedate", "plateplatform", "platenumber",                 
+                    "pangolin_lineage", "pangolin_probability", "pangolin_status",             
+                    "pangolin_note", "nextclade_clade", "nextclade_totalmissing",      
+                    "nextclade_completeness", "gisaid_strain", "gisaid_epi_isl",              
+                    "received_date", "position",          
+                    "platename", "plateposition", "samplesourcelocation",        
+                    "pangolearn_version", "pango_version",               
+                    "pangolin_version", "pangolin_conflict",     
+                    "nextclade_qcoverallscore", "nextclade_qcoverallstatus", "nextclade_totalmutations",    
+                    "nextclade_totalnonacgtns", 
+                    "data_quality_rule", "newest_pangolin_lineage", "newest_pangolin_date", "public_gisaid")
 
 
 #seq_list <- filter(seq_list, platenumber <= 49)

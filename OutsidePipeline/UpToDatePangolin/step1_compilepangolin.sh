2bf4af0ae31f797db69a3b38f200d0b9fd4dffec
Julie Gilbert <juliegil@m-h4tgx16rpn7c.wifi.med.umich.edu>
2022-11-09 10:14:58 -0500

    added comments

--- OutsidePipeline/UpToDatePangolin/step1_compilepangolin.sh
+++ OutsidePipeline/UpToDatePangolin/step1_compilepangolin.sh
@@ -2,11 +2,14 @@
 
 UNIQNAME="$1"
 
+# run code to create full compiled fasta file
 if [ "$UNIQNAME" = "juliegil" ]; then
   python3 /Users/$UNIQNAME/Documents/git_synced_code/SequenceCompilationCode/COVID19SequenceCompile/OutsidePipeline/UpToDatePangolin/compile_all_fastas.py
 else
-  python3 /Users/$UNIQNAME/Documents/Lauring_Lab/COVID19SequenceCompile/OutsidePipeline/UpToDatePangolin/compile_all_fastas.p
+  python3 /Users/$UNIQNAME/Documents/Lauring_Lab/COVID19SequenceCompile/OutsidePipeline/UpToDatePangolin/compile_all_fastas.py
 fi
 
+# move into dropbox location where full fasta file is written to
 cd '/Users/$UNIQNAME/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/4_SequenceSampleMetadata/SequenceOutcomes/pangolin/CompleteFastaUpToDate'
+# copy that full fasta file up into greatlakes
 scp complete_set.all.consensus.renamed.full.fasta $UNIQNAME@greatlakes-xfer.arc-ts.umich.edu:/nfs/turbo/umms-alauring/pangolin_refreshes/full_fasta_file_folder

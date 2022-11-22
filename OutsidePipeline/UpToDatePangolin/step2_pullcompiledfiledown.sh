dd6c33643857169f484be5eb083a657c329174d5
Julie Gilbert <juliegil@m-h4tgx16rpn7c.wifi.med.umich.edu>
2022-11-22 08:58:02 -0500

    finalizing full pango refresh pull down code

--- OutsidePipeline/UpToDatePangolin/step2_pullcompiledfiledown.sh
+++ OutsidePipeline/UpToDatePangolin/step2_pullcompiledfiledown.sh
@@ -5,10 +5,10 @@ TODAYDATE=$(date "+%Y%m%d")
 
 #echo $TODAYDATE
 # move into dropbox folder where complete fasta information is
-cd '/Users/$UNIQNAME/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/4_SequenceSampleMetadata/SequenceOutcomes/pangolin/CompleteFastaUpToDate'
+cd '/Users/'$UNIQNAME'/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/4_SequenceSampleMetadata/SequenceOutcomes/pangolin/CompleteFastaUpToDate'
 
 # move the old full pangolin file into archive
-mv lineage_report*.csv '/Users/$UNIQNAME/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/4_SequenceSampleMetadata/SequenceOutcomes/pangolin/CompleteFastaUpToDate/ARCHIVE/'
+mv lineage_report*.csv '/Users/'$UNIQNAME'/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/4_SequenceSampleMetadata/SequenceOutcomes/pangolin/CompleteFastaUpToDate/ARCHIVE/'
 
 # copy a new full pangolin file back down
 scp $UNIQNAME@greatlakes-xfer.arc-ts.umich.edu:/nfs/turbo/umms-alauring/pangolin_refreshes/pan_out/lineage_report.csv lineage_report_$TODAYDATE.csv

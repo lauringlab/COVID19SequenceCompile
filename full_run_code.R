695d0adc73c0f4e5cfb64823e5b6ccff0f1e1943
Julie Gilbert <juliegil@m-h4tgx16rpn7c.wifi.med.umich.edu>
2022-12-15 11:30:21 -0500

    running

--- full_run_code.R
+++ full_run_code.R
@@ -46,7 +46,7 @@ source(paste0(code_path, "pipeline_functions.R"))
 #<<<<<<< HEAD
 plate_name <- "20221115_SC2_Nanopore_Run_256"
 #=======
-plate_name <- "20220720_SC2_Illumina_Run_58"
+plate_name <- "20221115_SC2_Illumina_Run_71"
 #>>>>>>> 965c5720bf619c9fbd3a0ac0fa4ba776ee6d272f
 
 ################################################################################

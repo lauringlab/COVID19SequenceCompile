821a32c1b5a45eb44e24d59be539d88ef896d261
Julie Gilbert <juliegil@mswa0341.wifi.med.umich.edu>
2023-01-10 11:50:05 -0500

    updates

--- OutsidePipeline/ProcessingFASTA/prep_fasta_NumberTwo.py
+++ OutsidePipeline/ProcessingFASTA/prep_fasta_NumberTwo.py
@@ -8,7 +8,7 @@
 # use in git bash
 #c:/users/juliegil/appdata/local/programs/python/python38/python.exe C:/Users/juliegil/Documents/UofM_Work/SequenceCompilationCode/OutsidePipeline/ProcessingFASTA/prep_fasta_NumberTwo.py --prefix "C:/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/3_ProcessedGenomes/20220208_SC2_Nanopore_Run_117/20220208_SC2_Nanopore_Run_117"
 # python prep_fasta_NumberTwo.py --prefix 20210519_Nanopore_Run_26
-# python3 prep_fasta_NumberTwo.py --prefix "/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/3_ProcessedGenomes/20221027_SC2_Nanopore_Run_246/20221027_SC2_Nanopore_Run_246"
+# python3 prep_fasta_NumberTwo.py --prefix "/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/3_ProcessedGenomes/20221220_SC2_Nanopore_Run_274/20221220_SC2_Nanopore_Run_274"
 #"""
 
 # ======================= Import modules ======================
--- OutsidePipeline/gisaid_upload_file_creation.R
+++ OutsidePipeline/gisaid_upload_file_creation.R
@@ -14,8 +14,10 @@ checking_wd <- getwd()
 if (grepl("juliegil", checking_wd)){
   
   #code_path <- "/Users/juliegil/Documents/git_synced_code/SequenceCompilationCode/COVID19SequenceCompile/"
-  code_path <- "C:/Users/juliegil/Documents/UofM_Work/SequenceCompilationCode/"
-  starting_path <- "C:/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/"
+  #code_path <- "C:/Users/juliegil/Documents/UofM_Work/SequenceCompilationCode/"
+  #starting_path <- "C:/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/"
+  code_path <- "/Users/juliegil/Documents/git_synced_code/SequenceCompilationCode/COVID19SequenceCompile/"
+  starting_path <- "/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/"
   
 } else if (grepl("leighbaker", checking_wd)){
   code_path <- "/Users/leighbaker/Documents/Lauring_Lab/COVID19SequenceCompile/"
@@ -100,7 +102,7 @@ if (any(ff$sample_per_subject > 1)){
 
 ### uncomment this portion to remove those samples
 ### to remove these: 
-# ff <- filter(ff, sample_per_subject == 1 | subject_id %in% c("011718425", "022382986",
+#ff <- filter(ff, sample_per_subject == 1 | subject_id %in% c("011718425", "022382986",
 #                                                              "028140711", "045019901",
 #                                                              "101044617", "101472932"))
 #ff <- filter(ff, sample_per_subject == 1)

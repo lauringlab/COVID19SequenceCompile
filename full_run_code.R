bad3e8617d80954094495279657dac19ad3c3a0e
Julie Gilbert <juliegil@mswa0341.wifi.med.umich.edu>
2023-02-09 09:20:51 -0500

    reg

--- full_run_code.R
+++ full_run_code.R
@@ -149,7 +149,7 @@ source(paste0(code_path, "compile_components_code.R"))
 # INFLUENZA COMPONENT
 ################################################################################
 
-plate_name <- "20230102_IAV_Illumina_Run_35_SETCv2"
+plate_name <- "20230112_IAV_Illumina_Run_40_SETD"
 #plate_name <- "DATE_SC2_Illumina_Run_XX_E6440"
 
 ################################################################################

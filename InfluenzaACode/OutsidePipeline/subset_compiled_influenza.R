dd57512d8c15d9cf17ef9d8e8c2b24ca7bb3cd9b
Julie Gilbert <juliegil@mswa0341.wifi.med.umich.edu>
2023-05-30 08:19:01 -0400

    allowing mixed plate for iav

--- InfluenzaACode/OutsidePipeline/subset_compiled_influenza.R
+++ InfluenzaACode/OutsidePipeline/subset_compiled_influenza.R
@@ -47,29 +47,31 @@ missing_subject_id <- nrow(filter(seq_list2, subject_id == "" | is.na(subject_id
 missing_collection_date <- nrow(filter(seq_list2, coll_date == "" | is.na(as_date(coll_date)) | is.na(coll_date)))
 missing_received_source <- nrow(filter(seq_list2, received_source == "" | is.na(received_source)))
 
-if (missing_subject_id > 3){
+
+if (missing_subject_id > (96 - num_iavs_on_plate)){
   print("Check manifests for:")
   print(plate_name)
   stop("Subject IDs Missing")
 }
 
-if (missing_collection_date > 3){
+if (missing_collection_date > (96 - num_iavs_on_plate)){
   print("Check manifests for:")
   print(plate_name)
   stop("Collection Dates Missing")
 }
 
-if (missing_received_source > 3){
+if (missing_received_source > (96 - num_iavs_on_plate)){
   print("Check manifests for:")
   print(plate_name)
   stop("Received Sources Missing")
 }
 
-if (nrow(seq_list2) != 96){
-  print("There are not 96 samples on this plate:")
+if (nrow(seq_list2) != num_iavs_on_plate + 3){
+  print(paste0("There are not ", num_iavs_on_plate, " samples on this plate:"))
   print(plate_name)
   print(paste0("Plate rows = ", nrow(seq_list2)))
 }
+
 
 # for use for identifying missing manifests
 # out <- filter(seq_list2, subject_id == "")

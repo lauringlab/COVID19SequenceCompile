8992ff59dd9e9946b19f12ae43a81b341ecf8570
Julie Gilbert <juliegil@mswa0341.local>
2023-06-19 08:47:31 -0400

    run updates

--- full_run_code.R
+++ full_run_code.R
@@ -7,14 +7,14 @@
 checking_wd <- getwd()
 
 if (grepl("juliegil", checking_wd)){
-  starting_path <- "C:/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/"
-  #starting_path <- "/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/"
-  code_path <- "C:/Users/juliegil/Documents/UofM_Work/SequenceCompilationCode/"
-  #code_path <- "/Users/juliegil/Documents/git_synced_code/SequenceCompilationCode/COVID19SequenceCompile/"
-  batch_path <- "C:/Users/juliegil/Documents/UofM_Work/Lauring_Lab/Lab_Organization/AlertCode"
-  #batch_path <- "/Users/juliegil/Documents/LauringLab_Code/AlertCode"
-  influenza_path <- "C:/Users/juliegil/Documents/UofM_Work/SequenceCompilationCode/InfluenzaACode/"
-  #influenza_path <- "/Users/juliegil/Documents/git_synced_code/SequenceCompilationCode/COVID19SequenceCompile/InfluenzaACode/"
+  #starting_path <- "C:/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/"
+  starting_path <- "/Users/juliegil/Dropbox (University of Michigan)/MED-LauringLab/"
+  #code_path <- "C:/Users/juliegil/Documents/UofM_Work/SequenceCompilationCode/"
+  code_path <- "/Users/juliegil/Documents/git_synced_code/SequenceCompilationCode/COVID19SequenceCompile/"
+  #batch_path <- "C:/Users/juliegil/Documents/UofM_Work/Lauring_Lab/Lab_Organization/AlertCode"
+  batch_path <- "/Users/juliegil/Documents/LauringLab_Code/AlertCode"
+  #influenza_path <- "C:/Users/juliegil/Documents/UofM_Work/SequenceCompilationCode/InfluenzaACode/"
+  influenza_path <- "/Users/juliegil/Documents/git_synced_code/SequenceCompilationCode/COVID19SequenceCompile/InfluenzaACode/"
   
 } else if (grepl("leighbaker", checking_wd)){
   
@@ -164,7 +164,7 @@ source(paste0(code_path, "compile_components_code.R"))
 plate_name <- "20230402_IAV_Illumina_Run_56_SETD"
 #plate_name <- "DATE_SC2_Illumina_Run_XX_E6440"
 
-plate_name <- "20230607_IAV_Nanopore_Run_25"
+plate_name <- "20230608_IAV_Nanopore_Run_26"
 
 num_iavs_on_plate <- 93
 

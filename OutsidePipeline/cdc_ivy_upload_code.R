150cf83fec21acc228a3df560cbec0ad02549241
Gilbert <juliegil@UMICH.EDU>
2023-09-15 07:26:20 -0400

    ivy column naming edits

--- OutsidePipeline/cdc_ivy_upload_code.R
+++ OutsidePipeline/cdc_ivy_upload_code.R
@@ -153,6 +153,8 @@ ivy4 <- ivy4 %>% select(sample_id, subject_id, coll_date,
 names(ivy4)[names(ivy4) == 'subject_id'] <- 'study_id'
 
 
+ivy5$nextclade_sf456l_present <- ivy5$sf456l_present
+
 # change subject_id to study_id
 ivy5 <- ivy5 %>% select(sample_id, subject_id, coll_date,                    
                         flag, received_source, sitename, samplebarcode,                
@@ -163,14 +165,15 @@ ivy5 <- ivy5 %>% select(sample_id, subject_id, coll_date,
                         plateposition, samplesourcelocation, pangolearn_version,
                         pango_version, pangolin_version, nextclade_qcoverallscore, nextclade_qcoverallstatus, 
                         nextclade_totalmutations, nextclade_totalnonacgtns, 
-                        data_quality_rule, newest_pangolin_lineage, newest_pangolin_date)
+                        data_quality_rule, newest_pangolin_lineage, newest_pangolin_date, nextclade_sf456l_present)
 
 #SC2_NP_333, SC2_NP_334, SC2_NP_335
 
-ivy5 <- ivy5 %>% mutate(newest_pangolin_lineage = case_when(platename %in% c("20230720_SC2_Nanopore_Run_333", "20230720_SC2_Nanopore_Run_334", "20230720_SC2_Nanopore_Run_335") ~ "", 
-                                                            T ~ newest_pangolin_lineage), 
-                        newest_pangolin_date = case_when(platename %in% c("20230720_SC2_Nanopore_Run_333", "20230720_SC2_Nanopore_Run_334", "20230720_SC2_Nanopore_Run_335") ~ "", 
-                                                            T ~ newest_pangolin_date))
+# ivy5 <- ivy5 %>% mutate(newest_pangolin_lineage = case_when(platename %in% c("20230720_SC2_Nanopore_Run_333", "20230720_SC2_Nanopore_Run_334", "20230720_SC2_Nanopore_Run_335") ~ "", 
+#                                                             T ~ newest_pangolin_lineage), 
+#                         newest_pangolin_date = case_when(platename %in% c("20230720_SC2_Nanopore_Run_333", "20230720_SC2_Nanopore_Run_334", "20230720_SC2_Nanopore_Run_335") ~ "", 
+#                                                             T ~ newest_pangolin_date))
+
 #table(ivy5$newest_pangolin_lineage == "XBB.1.5")
 # ivy5_out <- filter(ivy5, platename %in% c("20230124_SC2_Illumina_Run_78", "20230330_SC2_Illumina_Run_95", "20230523_SC2_Illumina_Run_106"))
 # ivy5_out[, c(11:33)] <- ""

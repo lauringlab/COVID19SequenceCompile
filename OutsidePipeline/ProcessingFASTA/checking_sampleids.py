6f79d1c0b2da762b6ac4a361a1b67ca3305c756f
Leigh Papalambros <leighbak@mac-rp4qxy5v2f.local>
2024-10-04 09:26:16 -0400

    updated dropbox path

--- OutsidePipeline/ProcessingFASTA/checking_sampleids.py
+++ OutsidePipeline/ProcessingFASTA/checking_sampleids.py
@@ -24,9 +24,9 @@ if "juliegil" in current_user:
 elif "leighbaker" in current_user:
     meta_file_location = "/Users/leighbaker/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/3_ProcessedGenomes/" + plate_run_name + "/"
 elif "leighbak" in current_user:
-    meta_file_location = "/Users/leighbak/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/3_ProcessedGenomes/" + plate_run_name + "/"
+    meta_file_location = "/Users/leighbak/University of Michigan Dropbox/MED-LauringLab/SEQUENCING/SARSCOV2/3_ProcessedGenomes/" + plate_run_name + "/"
 elif "chbl" in current_user:
-    meta_file_location = "/Users/chbl/Dropbox (University of Michigan)/MED-LauringLab/SEQUENCING/SARSCOV2/3_ProcessedGenomes/" + plate_run_name + "/"    
+    meta_file_location = "/Users/chbl/University of Michigan Dropbox/MED-LauringLab/SEQUENCING/SARSCOV2/3_ProcessedGenomes/" + plate_run_name + "/"    
 else:
     print("User not recognized.")
 

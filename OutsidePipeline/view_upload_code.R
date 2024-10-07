e101a90a1987c5f1612fb65feece3a91d0bfe8bc
Leigh Papalambros <leighbak@mac-rp4qxy5v2f.local>
2024-10-07 13:34:48 -0400

    updated dropbox path

--- OutsidePipeline/view_upload_code.R
+++ OutsidePipeline/view_upload_code.R
@@ -9,7 +9,7 @@ library(lubridate)
 
 ################################################################################
 
-starting_path <- "/Users/leighbak/Dropbox (University of Michigan)/MED-LauringLab/"
+starting_path <- "/Users/leighbak/University of Michigan Dropbox/MED-LauringLab/"
 outputLOC <- paste0(starting_path, "SEQUENCING/SARSCOV2/4_SequenceSampleMetadata/FinalSummary/VIEW_UPLOADS/")
 
 seq_list <- read.csv(paste0(starting_path, "SEQUENCING/SARSCOV2/4_SequenceSampleMetadata/FinalSummary/full_compiled_data.csv"), colClasses = "character")
